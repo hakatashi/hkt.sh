a7bdc75ac4de7da1bb38913dea6d4c8b52309cb7
Koki Takahashi <hakatasiloving@gmail.com>
2020-05-11 04:59:48 +0900

    Remove unneccesarry codes

--- home/main.go
+++ home/main.go
@@ -3,45 +3,15 @@ package main
 import (
 	"bytes"
 	"encoding/json"
-	"errors"
 	"html/template"
-	"io/ioutil"
-	"net/http"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-var (
-	// DefaultHTTPGetAddress Default Address
-	DefaultHTTPGetAddress = "https://checkip.amazonaws.com"
-
-	// ErrNoIP No IP found in response
-	ErrNoIP = errors.New("No IP in HTTP response")
-
-	// ErrNon200Response non 200 status code in response
-	ErrNon200Response = errors.New("Non 200 Response found")
-)
+var ()
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	resp, err := http.Get(DefaultHTTPGetAddress)
-	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
-	}
-
-	if resp.StatusCode != 200 {
-		return events.APIGatewayProxyResponse{}, ErrNon200Response
-	}
-
-	ip, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
-	}
-
-	if len(ip) == 0 {
-		return events.APIGatewayProxyResponse{}, ErrNoIP
-	}
-
 	body, err := json.MarshalIndent(request.Headers, "", "\t")
 	if err != nil {
 		return events.APIGatewayProxyResponse{}, err
